efc49a1ef88c8b33586590c25ed2bae52b708286
Jathu03 <129855031+Jathu03@users.noreply.github.com>
2025-05-09 23:04:28 +0530

    Update CostModel.h

--- src/autoschedulers/adams2019/CostModel.h
+++ src/autoschedulers/adams2019/CostModel.h
@@ -1,40 +1,32 @@
-#ifndef HALIDE_COST_MODEL_H
-#define HALIDE_COST_MODEL_H
+/*
+  CostModel.h: Abstract base class for cost models in the Halide autoscheduler.
+  Defines the interface for evaluating the cost of a schedule state.
+*/
+
+#ifndef HALIDE_AUTOSCHEDULER_COST_MODEL_H
+#define HALIDE_AUTOSCHEDULER_COST_MODEL_H
 
 #include "FunctionDAG.h"
-#include "Featurization.h"
-#include "TreeRepresentation.h"
-#include <memory>
-#include <string>
+#include "Halide.h"
 
 namespace Halide {
 namespace Internal {
 namespace Autoscheduler {
 
-struct Adams2019Params {
-    std::string weights_path;
-    std::string model_type = "SimpleLSTM";
-};
+// Forward declaration of TreeRepresentation
+class TreeRepresentation;
 
 class CostModel {
 public:
     virtual ~CostModel() = default;
 
-    virtual void set_pipeline_features(const FunctionDAG &dag,
-                                      const Adams2019Params &params) = 0;
-
-    virtual void enqueue(const FunctionDAG &dag,
-                         const StageMapOfScheduleFeatures &schedule_feats,
-                         const TreeRepresentation &tree_repr,
-                         double *cost_ptr) = 0;
-
-    virtual void evaluate_costs() = 0;
-
-    virtual void reset() = 0;
+    // Evaluate the cost of a given schedule state for a function DAG.
+    // Returns the estimated cost as a double.
+    virtual double evaluate_cost(const IntrusivePtr<State> &state, const FunctionDAG &dag) = 0;
 };
 
-} // namespace Autoscheduler
-} // namespace Internal
-} // namespace Halide
+}  // namespace Autoscheduler
+}  // namespace Internal
+}  // namespace Halide
 
-#endif // HALIDE_COST_MODEL_H
+#endif  // HALIDE_AUTOSCHEDULER_COST_MODEL_H
