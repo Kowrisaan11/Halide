322089acf89c0fc6b3f24a8abddc02aef4d02c46
Jathu03 <129855031+Jathu03@users.noreply.github.com>
2025-05-10 18:20:55 +0530

    Update AutoSchedule.h

--- src/autoschedulers/adams2019/AutoSchedule.h
+++ src/autoschedulers/adams2019/AutoSchedule.h
@@ -1,34 +1,18 @@
-#pragma once
-
-#include "FunctionDAG.h"
 #include "CostModel.h"
+#include "Featurization.h"
+#include "FunctionDAG.h"
+#include "Halide.h"
+#include "PerfectHashMap.h"
 #include <vector>
-#include <random>
 
 namespace Halide {
 namespace Internal {
 namespace Autoscheduler {
 
-// Forward declaration
-struct Adams2019Params;
-struct AutoSchedulerResults;
-class Cache;
-struct State;
-struct LoopNest;
-struct CachingOptions;
+typedef PerfectHashMap<FunctionDAG::Node::Stage, ScheduleFeatures> StageMapOfScheduleFeatures;
 
-// Top-level schedule generation
-void generate_schedule(const std::vector<Function> &outputs,
-                       const Target &target,
-                       const Adams2019Params &params,
-                       AutoSchedulerResults *auto_scheduler_results);
-
-// Run search and apply a schedule; optionally return featurization
-void find_and_apply_schedule(FunctionDAG &dag,
-                             const std::vector<Function> &outputs,
-                             const Adams2019Params &params,
-                             CostModel *cost_model,
-                             StageMapOfScheduleFeatures *schedule_features);
+void find_and_apply_schedule(FunctionDAG &dag, const std::vector<Function> &outputs, const Adams2019Params &params,
+                             CostModel *cost_model, int beam_size, StageMapOfScheduleFeatures *schedule_features);
 
 }  // namespace Autoscheduler
 }  // namespace Internal
